0357a10bfe5f9d19c18dee01de1155ce99affbf8
Kyrylo Skok <116465962+SkokKI@users.noreply.github.com>
2023-04-16 18:50:11 +0300

    Add TestDriver and ModulesSkok

---
+++ lab09/prj/TestDriver/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<ModulesSkok.h>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+    cout<<"TEST 9_1\n";
+    for(int i = 0;i<10;i++)
+    {
+        cout<<"Test "<<i+1<<"\n";
+        pow_wind(0);
+    }
+    system("PAUSE");
+    system("cls");
+    cout<<"TEST 9_2\n";
+    for(int i = 0;i<10;i++)
+    {
+        cout<<"Test "<<i+1<<"\n";
+        size_helmet(0);
+    }
+    system("PAUSE");
+    system("cls");
+    cout<<"TEST 9_3\n";
+     for(int i = 0;i<10;i++)
+    {
+        cout<<"Test "<<i+1<<"\n";
+        bit_15(0);
+        cout<<"\n";
+    }
+    return 0;
+}
