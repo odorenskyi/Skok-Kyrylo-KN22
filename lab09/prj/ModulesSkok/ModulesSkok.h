46e400a8995009478c00d4d4ec12aefd5b287aba
Kyrylo Skok <116465962+SkokKI@users.noreply.github.com>
2023-04-20 18:55:53 +0300

    Delete lab09/prj directory

--- lab09/prj/ModulesSkok/ModulesSkok.h
+++
@@ -1,8 +0,0 @@
-#ifndef MODULESSKOK_H_INCLUDED
-#define MODULESSKOK_H_INCLUDED
-
-int s_calculation(int x,int y,int z);
-double pow_wind(double speed_wind);
-int size_helmet(int size_h);
-int bit_15(int my_number);
-#endif
