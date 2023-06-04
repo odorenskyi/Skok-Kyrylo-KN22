febd3928b1d59c04faf28148a8cb91c4a16b4b35
Kyrylo Skok <116465962+SkokKI@users.noreply.github.com>
2023-06-04 20:02:45 +0300

    Додана папка prj

---
+++ lab11/prj/struct_type_project_12.h
@@ -0,0 +1,19 @@
+#ifndef STRUCT_TYPE_PROJECT_12_H_INCLUDED
+#define STRUCT_TYPE_PROJECT_12_H_INCLUDED
+#include <string>
+#include <vector>
+using nemespace std;
+struct Benefits
+{
+    int code_ben;
+    string content_ben;
+    string document_ben;
+    string num_doc_ben;
+    string date_doc_ben;
+    string date_start_ben;
+    string date_end_ben;
+    string destination_sign_ben;
+    string coment_ben;
+};
+vector <Benefits> note;
+#endif // STRUCT_TYPE_PROJECT_12_H_INCLUDED
